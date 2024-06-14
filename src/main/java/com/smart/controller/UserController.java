22f4615a53bb287de1f55ca2a8269975cfcc2a61
Rahul <rahulbains04@gmail.com>
2024-06-14 18:03:39 +0530

    Contact viewer page added

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -31,6 +33,9 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ContactRepository contactRepository;
 	
 	//method for adding common data to the response
 	@ModelAttribute 
@@ -120,6 +125,24 @@ public class UserController {
 		
 		return "normal/add_contact";
 		
+	}
+	
+	//show contacts handler
+	@GetMapping("/show-contacts")
+	public String showContacts(Model m,Principal principal)
+	{	
+		m.addAttribute("title","Show user contacts");
+		
+		//contact list yield
+		String userName = principal.getName();
+		User user = userRepository.getUserByUserName(userName);
+	
+		List<Contact> contacts = contactRepository.findContactsByUser(user.getId());
+		
+		m.addAttribute("contacts",contacts);
+		
+		
+		return "normal/show_contacts";
 	}
 	
 	
